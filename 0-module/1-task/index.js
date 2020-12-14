ff59f81b699c9315af4779d1bb5c736e59bf3cd1
Iurii <quzelko@gmail.com>
2020-12-15 00:22:31 +0100

    First task

--- 0-module/1-task/index.js
+++ 0-module/1-task/index.js
@@ -5,5 +5,35 @@
  * @returns {number}
  */
 function sum(m, n) {
-  // ваш код...
+m = m || 0; // the default value for the first argument
+n = n || 0; // the default value for the second argument
+
+let total, symbolsAfterPoint;
+symbolsAfterPoint = 0;   
+
+if((Number.isInteger(m) && Number.isInteger(n)) != true){  // check for fractional numbers
+
+  let nonInteger1, nonInteger2;
+  nonInteger1 = nonInteger2 = 0;
+
+  if(Number.isInteger(m) != true){
+    nonInteger1 = (m.toString().length) - (m.toString().lastIndexOf('.') + 1);
+  }
+
+  if(Number.isInteger(n) != true){
+    nonInteger2 = (n.toString().length) - (n.toString().lastIndexOf('.') + 1);
+  }
+
+  symbolsAfterPoint = Math.max(nonInteger1,nonInteger2);  
+}
+
+if(symbolsAfterPoint == 0){
+  total = m + n;
+} 
+else{
+  total = +(m + n).toFixed(symbolsAfterPoint); 
+}
+
+return total;
+
 }
